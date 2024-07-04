54a20c6c05b9f04c7646ee01bea40ff8e554e351
RaphLrg <raphael.largeau@gmail.com>
2024-07-04 10:10:13 -0400

    amelioration des parametres

--- proc_vision_ros2/vision_node.py
+++ proc_vision_ros2/vision_node.py
@@ -6,6 +6,8 @@ import numpy as np
 from sonia_common_ros2.msg import VisionClass, AiActivationService
 from items_robosub import ItemsRobosub
 
+MODEL = 'models/model-1-yolov8n.pt'
+
 class VisionNode(Node):
 
     def __init__(self):
@@ -15,7 +17,7 @@ class VisionNode(Node):
         self.__ai_activation_sub = self.create_subscription(AiActivationService, "proc_vision/ai_activation", self.__ai_activation_callback, 10)
         self.__front_cam_sub = self.create_subscription(Image, "camera_array/front/image_raw", self.__img_front_callback, 10)
         self.__bottom_cam_sub = self.create_subscription(Image, "camera_array/bottom/image_raw", self.__img_bottom_callback, 10)
-        self.model = YOLO('models/model-1-yolov8n.pt')
+        self.model = YOLO(MODEL)
         self.__classification_front_pub = self.create_publisher(VisionClass, "proc_vision/front/classification", 10)
         self.__classification_bottom_pub = self.create_publisher(VisionClass, "proc_vision/bottom/classification", 10)
 

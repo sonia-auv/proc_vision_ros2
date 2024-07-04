5c6068b99f3bddc09d7aa47d473bd41ce89f5387
RaphLrg <raphael.largeau@gmail.com>
2024-07-04 00:13:19 -0400

    nouveau model meilleur

--- proc_vision_ros2/vision_node.py
+++ proc_vision_ros2/vision_node.py
@@ -15,7 +15,7 @@ class VisionNode(Node):
         self.__ai_activation_sub = self.create_subscription(AiActivationService, "proc_vision/ai_activation", self.__ai_activation_callback, 10)
         self.__front_cam_sub = self.create_subscription(Image, "camera_array/front/image_raw", self.__img_front_callback, 10)
         self.__bottom_cam_sub = self.create_subscription(Image, "camera_array/bottom/image_raw", self.__img_bottom_callback, 10)
-        self.model = YOLO('models/yolov8n.pt')
+        self.model = YOLO('models/model-1-yolov8n.pt')
         self.__classification_front_pub = self.create_publisher(VisionClass, "proc_vision/front/classification", 10)
         self.__classification_bottom_pub = self.create_publisher(VisionClass, "proc_vision/bottom/classification", 10)
 
@@ -45,7 +45,7 @@ class VisionNode(Node):
 
     def __img_detection(self, msg: Image):
         img = np.array(msg.data).reshape((400,600,3))
-        results = self.model(img, imgsz=640, conf=0.5, verbose=False)
+        results = self.model(img, imgsz=[600, 400], conf=0.5, verbose=False)
         for res in results:
             detection_count = res.boxes.shape[0]
             for i in range(detection_count):

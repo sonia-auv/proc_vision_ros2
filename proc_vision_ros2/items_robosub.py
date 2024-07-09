3139cc98b22c1823e33bb6a0c854d64edd9f70ff
Ewan Féminis <f-ewan@outlook.com>
2024-07-09 17:19:36 -0400

    Fix distance target

--- proc_vision_ros2/items_robosub.py
+++ proc_vision_ros2/items_robosub.py
@@ -8,6 +8,7 @@ class ItemsRobosub():
         self.constante = self.args['constante_camera']
 
     def get_dist(self, h, cls_name, target_index=1):
+        print(cls_name)
         try:
             if cls_name == 'torpille_target':
                 dist = self.get_dist_target(h, target_index)
@@ -19,8 +20,7 @@ class ItemsRobosub():
         
     def get_dist_target(self, h, index_target):
         try:
-            target = self.args['torpille_target'][index_target]
-            dist = target['h'] / (h * self.constante)
+            dist = self.args['torpille_target'][index_target]['h'] / (h * self.constante)
             return dist
         except:
             return -1

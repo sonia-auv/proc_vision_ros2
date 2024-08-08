94d18b7474efff9c02c7fd2c3875255d33bfd16b
Nemo <log.club.sonia@etsmtl.net>
2024-08-08 04:02:19 -0400

    Fix detection overlap and remove duplicate

--- proc_vision_ros2/vision_node.py
+++ proc_vision_ros2/vision_node.py
@@ -26,7 +26,7 @@ MODELS = ['model-1-yolov8n.pt',
           'carriere_zac_denise_yucca_sim_v10.pt',
           'robosub_v8_1.pt',
           'robosub_most_recent.pt']
-MODEL_INDEX = 11
+MODEL_INDEX = 7
 OUTPUT_DIR = 'output_ai/'
 SAVE_OUTPUT = False
 PUBLISH_OUTPUT = True
@@ -37,8 +37,8 @@ def iou(d1, d2):
     xB = min(d1.right, d2.right)
     yB = min(d1.bottom, d2.bottom)
     inter = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-    area1 = (d2.left - d1.left + 1) * (d2.top - d1.top + 1)
-    area2 = (d2.right - d1.right + 1) * (d2.bottom - d1.bottom + 1)
+    area1 = (d1.right - d1.left + 1) * (d1.bottom - d1.top + 1)
+    area2 = (d2.right - d2.left + 1) * (d2.bottom - d2.top + 1)
     return (inter / float(area1 + area2 - inter))
 
 
@@ -56,7 +56,8 @@ class VisionNode():
         self.model = YOLO(MODEL_DIR + MODELS[MODEL_INDEX])
         self.__classification_front_pub = rospy.Publisher("proc_vision/front/classification", DetectionArray, queue_size=10)
         self.__classification_bottom_pub = rospy.Publisher("proc_vision/bottom/classification", DetectionArray, queue_size=10)
-        self.__image_output_pub = rospy.Publisher("proc_vision/ai_output", Image, queue_size=10)
+        self.__image_output_front_pub = rospy.Publisher("proc_vision/ai_output/front", Image, queue_size=10)
+        self.__image_output_bottom_pub = rospy.Publisher("proc_vision/ai_output/bottom", Image, queue_size=10)
         self.previous_front_results = [[], [], [], [], []]
         self.previous_bottom_results = [[], [], [], [], []]
 
@@ -102,8 +103,8 @@ class VisionNode():
             img[:,:,2] = temp
 
             detection_array = DetectionArray()
-            for detected_obj in self.__img_detection(img):
-                detection_array.detected_object.append(detected_obj, 'front')
+            for detected_obj in self.__img_detection(img, 'front'):
+                detection_array.detected_object.append(detected_obj)
             
             self.__classification_front_pub.publish(detection_array)
 
@@ -133,15 +134,56 @@ class VisionNode():
             
             self.__classification_bottom_pub.publish(detection_array)
 
-    def __remove_double(self, detections):
-        for d1 in detections:
-            for d2 in detections:
-                if d1 != d2 and d1.cls == d2.cls:
+    def __remove_double(self, detections:list):
+        for d1 in detections.copy():
+            for d2 in detections.copy():
+                if d1 != d2 and d1.class_name == d2.class_name:
                     if iou(d1, d2) > .3:
-                        if d1.conf < d2.conf:
-                            detections.remove(d1)
-                        else:
-                            detections.remove(d2)
+                        try:
+                            if d1.confidence < d2.confidence:
+                                detections.remove(d1)
+                            else:
+                                detections.remove(d2)
+                        except:
+                            pass
+
+    def __save_and_publish(self, img, detections, camera):
+        for res in detections:
+            cv2.putText(img, 
+                        res.class_name, 
+                        (int((res.left+1)),
+                        int((res.top-10))), 
+                        cv2.FONT_HERSHEY_PLAIN, 
+                        .7, (0,0,255), 1, 1)
+            cv2.putText(img, 
+                        "{:.1f}%".format(res.confidence*100), 
+                        (int((res.left+1)),
+                        int((res.top-1))), 
+                        cv2.FONT_HERSHEY_PLAIN, 
+                        .7, (0,0,255), 1, 1)
+            cv2.rectangle(img, 
+                        (int(res.left),int(res.top)), 
+                        (int(res.right),int(res.bottom)), 
+                        (0,0,255), 1)
+            
+        if SAVE_OUTPUT and len(detections) != 0:
+            cv2.imwrite(OUTPUT_DIR+'pred_'+str(int(1000*time()))+'.jpg', 
+                        img)
+
+        if PUBLISH_OUTPUT:
+            image_out = Image()
+            image_out.height=400
+            image_out.width=600
+            image_out.encoding="bgr8"
+            image_out.is_bigendian=0
+            image_out.step=1800
+            image_out.data = img.reshape(720000).tolist()
+
+            if camera == 'front':
+                self.__image_output_front_pub.publish(image_out)
+
+            if camera == 'bottom':
+                self.__image_output_bottom_pub.publish(image_out)
 
     def __img_detection(self, img, camera='front'):
         results_now = self.model(img, imgsz=[608, 416], conf=0.5, verbose=False)
@@ -155,6 +197,7 @@ class VisionNode():
             self.previous_bottom_results.insert(0, results_now)
             self.previous_bottom_results.pop(len(self.previous_bottom_results)-1)
             previous_results = self.previous_bottom_results
+
         for results in previous_results:
             for res in results:
                 detection_count = res.boxes.shape[0]
@@ -178,40 +221,12 @@ class VisionNode():
                     classification.distance = float(items.get_dist(y2 - y1, name))
                     classification.confidence = float(res.boxes.conf[i].item())
                     detections.append(classification)
+                    
+        self.__remove_double(detections)
 
-                    if SAVE_OUTPUT or PUBLISH_OUTPUT:
-                        cv2.putText(img, 
-                                    name, 
-                                    (int((x1+1)),
-                                    int((y1-10))), 
-                                    cv2.FONT_HERSHEY_PLAIN, 
-                                    .7, (0,0,255), 1, 1)
-                        cv2.putText(img, 
-                                    "{:.1f}%".format(res.boxes.conf[i].item()*100), 
-                                    (int((x1+1)),
-                                    int((y1-1))), 
-                                    cv2.FONT_HERSHEY_PLAIN, 
-                                    .7, (0,0,255), 1, 1)
-                        cv2.rectangle(img, 
-                                    (int(x1),int(y1)), 
-                                    (int(x2),int(y2)), 
-                                    (0,0,255), 1)
-                     
-        if SAVE_OUTPUT and detection_count != 0:
-            cv2.imwrite(OUTPUT_DIR+'pred_'+str(int(1000*time()))+'.jpg', 
-                        img)
+        if SAVE_OUTPUT or PUBLISH_OUTPUT:
+            self.__save_and_publish(img, detections, camera)
 
-        if PUBLISH_OUTPUT:
-            image_out = Image()
-            image_out.height=400
-            image_out.width=600
-            image_out.encoding="bgr8"
-            image_out.is_bigendian=0
-            image_out.step=1800
-            image_out.data = img.reshape(720000).tolist()
-            self.__image_output_pub.publish(image_out)
-
-        detections = self.__remove_double(detections)
         return detections
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -12,15 +12,9 @@ setup(
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
-    zip_safe=True,
     maintainer='sonia2',
     maintainer_email='log.sonia.club@etsmtl.ca',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'proc_vision_ros2 = proc_vision_ros2.main:main'
-        ],
-    },
 )

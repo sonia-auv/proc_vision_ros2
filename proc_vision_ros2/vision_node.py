cd1aff10302b1efb38fc8cdc8158e24d2dfa1bf8
Sonia-Nemo <log.sonia.club@etsmtl.ca>
2025-05-17 15:47:49 -0400

    mise à jour à tester avec l'input des caméras

--- proc_vision_ros2/vision_node.py
+++ proc_vision_ros2/vision_node.py
@@ -8,16 +8,14 @@ import numpy as np
 import cv2
 import os
 from time import time
-from sonia_common_ros2.msg import VisionClass
+from sonia_common_ros2.msg import Detection
 from sonia_common_ros2.srv import AiActivationService
 from .items_robosub import ItemsRobosub
 
-MODEL_DIR = '/home/sonia/ssd/ros2_sonia_ws/src/proc_vision_ros2/models/'
-MODELS = ['model-1-yolov8n.pt', 
-          'model-2-yolov8n.pt', 
-          'yolov10-1-700.pt']
-MODEL_INDEX = 2
-OUTPUT_DIR = '/home/sonia/ssd/output_ai/'
+# MODEL_DIR = '/home/sonia/ssd/ros2_sonia_ws/src/proc_vision_ros2/models/'
+# OUTPUT_DIR = '/home/sonia/ssd/output_ai/'
+MODEL_DIR = '/home/sonia2/ros2_sonia_ws/src/proc_vision_ros2/models/'
+OUTPUT_DIR = '/home/sonia2/output_ai/'
 SAVE_OUTPUT = True
 
 class VisionNode(Node):
@@ -26,26 +24,40 @@ class VisionNode(Node):
         super().__init__("vision_node")
         self.camera_front = False
         self.camera_bottom = False
+        self.declare_parameter("models") 
         self.__ai_activation_sub = self.create_service(AiActivationService, "proc_vision/ai_activation", self.__ai_activation_callback)
         self.__front_cam_sub = self.create_subscription(Image, "camera_array/front/image_raw/compressed", self.__img_front_callback, 10)
         self.__bottom_cam_sub = self.create_subscription(Image, "camera_array/bottom/image_raw", self.__img_bottom_callback, 10)
-        self.model = YOLO(MODEL_DIR + MODELS[MODEL_INDEX])
-        self.__classification_front_pub = self.create_publisher(VisionClass, "proc_vision/front/classification", 10)
-        self.__classification_bottom_pub = self.create_publisher(VisionClass, "proc_vision/bottom/classification", 10)
+        model_front_name = self.get_parameter("models").get_parameter_value().string_array_value[1]
+        model_bottom_name = self.get_parameter("models").get_parameter_value().string_array_value[1]
+        self.model_front = YOLO(os.path.join(MODEL_DIR, model_front_name))
+        self.model_bottom = YOLO(os.path.join(MODEL_DIR, model_bottom_name))
+        self.__classif_front_pub = self.create_publisher(Detection, "proc_vision/front/classif", 10)
+        self.__classif_bottom_pub = self.create_publisher(Detection, "proc_vision/bottom/classif", 10)
         if SAVE_OUTPUT:
             if not os.path.exists(OUTPUT_DIR):
                 os.makedirs(OUTPUT_DIR)
 
     def __ai_activation_callback(self, request, response):
-        if request.ai_activation == AiActivationService.Request.FRONT:
+        if request.camera_choice == AiActivationService.Request.FRONT:
             self.camera_front = True
             self.camera_bottom = False
-        elif request.ai_activation == AiActivationService.Request.BOTTOM:
+            if request.model_choice >= 0:
+                model_name = self.get_parameter("models").get_parameter_value().string_array_value[request.model_choice]
+                self.model_front = YOLO(os.path.join(MODEL_DIR, model_name))
+        elif request.camera_choice == AiActivationService.Request.BOTTOM:
             self.camera_front = False
             self.camera_bottom = True
-        elif request.ai_activation == AiActivationService.Request.BOTH:
+            if request.model_choice >= 0:
+                model_name = self.get_parameter("models").get_parameter_value().string_array_value[request.model_choice]
+                self.model_bottom = YOLO(os.path.join(MODEL_DIR, model_name))
+        elif request.camera_choice == AiActivationService.Request.BOTH:
             self.camera_front = True
             self.camera_bottom = True
+            if request.model_choice >= 0:
+                model_name = self.get_parameter("models").get_parameter_value().string_array_value[request.model_choice]
+                self.model_front = YOLO(os.path.join(MODEL_DIR, model_name))
+                self.model_bottom = YOLO(os.path.join(MODEL_DIR, model_name))
         else:
             self.camera_front = False
             self.camera_bottom = False
@@ -55,41 +67,60 @@ class VisionNode(Node):
     def __img_front_callback(self, msg: Image):
         self.get_logger().info("IMG FRONT received!!")
         if self.camera_front:
-            for detected_obj in self.__img_detection(msg):
-                self.__classification_front_pub.publish(detected_obj)
+            for detected_obj in self.__img_detection(msg, self.model_front):
+                self.__classif_front_pub.publish(detected_obj)
 
     def __img_bottom_callback(self, msg: Image):
         self.get_logger().info("IMG BOTTOM received!!")
         if self.camera_bottom:
-            for detected_obj in self.__img_detection(msg):
-                self.__classification_bottom_pub.publish(detected_obj)
+            for detected_obj in self.__img_detection(msg, self.model_bottom):
+                self.__classif_bottom_pub.publish(detected_obj)
 
-    def __img_detection(self, msg: Image):
+    def __manage_oriented_boxes(self, i: int, result, classif: Detection):
+        box = result.boxes.xyxy[i].cpu().numpy()
+        classif.top_left_x = float(box[0])
+        classif.top_left_y = float(box[1])
+        classif.top_right_x = float(box[2])
+        classif.top_right_y = float(box[3])
+        classif.bottom_right_x = float(box[4])
+        classif.bottom_right_y = float(box[5])
+        classif.bottom_left_x = float(box[6])
+        classif.bottom_left_y = float(box[7])
+        classif.confidence = float(result.boxes.conf[i].item())
+        return classif
+
+    def __manage_boxes(self, i: int, result, classif: Detection):
+        box = result.obb.xyxyxyxy[i].cpu().numpy()
+        classif.top_left_x = float(box[0])
+        classif.top_left_y = float(box[1])
+        classif.top_right_x = float(box[0])
+        classif.top_right_y = float(box[3])
+        classif.bottom_right_x = float(box[2])
+        classif.bottom_right_y = float(box[3])
+        classif.bottom_left_x = float(box[2])
+        classif.bottom_left_y = float(box[1])
+        classif.confidence = float(result.obb.conf[i].item())
+        return classif
+
+    def __img_detection(self, msg: Image, model: YOLO):
+        # TODO check and manage case with rgbxyz images
         img = np.array(msg.data).reshape((400,600,3))
-        results = self.model(img, imgsz=[600, 400], conf=0.5, verbose=False)
+        results = model(img, imgsz=[600, 400], conf=0.5, verbose=False)
         detections = []
         for res in results:
             detection_count = res.boxes.shape[0]
             for i in range(detection_count):
                 cls = int(res.boxes.cls[i].item())
                 name = res.names[cls]
-                box = res.boxes.xyxy[i].cpu().numpy()
-                x1 = box[0]
-                y1 = box[1]
-                x2 = box[2]
-                y2 = box[3]
-                w = x1 - x2
-                h = y1 - y2
-                items = ItemsRobosub()
-                classification = VisionClass()
-                classification.origin.x = float(x1)
-                classification.origin.y = float(y1)
-                classification.endpoint.x = float(x2)
-                classification.endpoint.y = float(y2)
-                classification.classification = name
-                classification.distance = float(items.get_dist(y2 - y1, name))
-                classification.confidence = float(res.boxes.conf[i].item())
-                detections.append(classification)
+                # TODO
+                if res.boxes is not None:
+                    classif = self.__manage_boxes(i, res)
+                else:
+                    classif = self.__manage_oriented_boxes(i, res)
+                classif = Detection()
+                classif.classif = name
+                # classif.distance = np.median() TODO
+                detections.append(classif)
 
                 if SAVE_OUTPUT:
                     cv2.putText(img, 
@@ -99,7 +130,7 @@ class VisionNode(Node):
                                 cv2.FONT_HERSHEY_PLAIN, 
                                 .7, (0,0,255), 1, 1)
                     cv2.putText(img, 
-                                "{:.1f}%".format(conf), 
+                                "{:.1f}%".format(classif.confidence), 
                                 (int((x1+5)),
                                 int((y2+10)/2)), 
                                 cv2.FONT_HERSHEY_PLAIN, 

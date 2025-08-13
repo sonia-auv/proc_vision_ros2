e7ea2222301d49ae5fd990e14f9f523669301c48
Erwanmanach <Manchoteu@aol.com>
2025-08-13 18:03:19 -0400

    first try deep map

--- proc_vision_ros2/node_to_see.py
+++ proc_vision_ros2/node_to_see.py
@@ -15,6 +15,8 @@ else:
     OUTPUT_DIR = '/home/sonia/output_ai/'
 SAVE_OUTPUT = False
 
+AREA_OF_SEE = 2
+
 class NodeTosee(Node):
 
     def __init__(self):
@@ -32,8 +34,8 @@ class NodeTosee(Node):
         if not os.path.exists(OUTPUT_DIR):
             os.makedirs(OUTPUT_DIR)
 
-    def __img_front_callback(self, msg: Image):
-        self.__front_list.append(np.frombuffer(msg.data, np.uint8))
+    def __img_front_callback(self, msg: CompressedImage):
+        self.__front_list.append(cv2.imdecode(np.frombuffer(msg.data, np.uint8), cv2.IMREAD_COLOR))
 
     def __img_bottom_callback(self, msg: Image):
         self.__bottom_list.append(np.frombuffer(msg.data, np.uint8))
@@ -88,28 +90,25 @@ class NodeTosee(Node):
                         image) 
         self.__classif_front_pub.publish(image)
 
-    def __get_depth(self, msg: Image):
-        self.__deep_list.append((np.frombuffer(msg.data, np.uint8),msg.width,msg.height))
-        image = self.__front_list[-1]
-        self.__front_list = [image]
-        for res in msg.detected_object:
-            cv2.putText(image, 
-                        res.class_name, 
-                        (int((res.top_left_x+5)),
-                        int((res.bottom_right_y-10)/2)), 
-                        cv2.FONT_HERSHEY_PLAIN, 
-                        .7, (0,0,255), 1, 1)
-            cv2.putText(image, 
-                        "{:.1f}%".format(res.confidence), 
-                        (int((res.top_left_x+5)),
-                        int((res.bottom_right_y+10)/2)), 
-                        cv2.FONT_HERSHEY_PLAIN, 
-                        .7, (0,0,255), 1, 1)
-            cv2.rectangle(image, 
-                        (int(res.top_left_x),int(res.top_left_y)), 
-                        (int(res.bottom_right_x),int(res.bottom_right_y)), 
-                        (0,0,255), 1)
-        if SAVE_OUTPUT:
-            cv2.imwrite(OUTPUT_DIR+'pred_front_'+str(int(1000*datetime.now().time()))+'.jpg', 
-                        image) 
-        self.__classif_front_pub.publish(image)
+    def __get_depth(self, msg: CompressedImage):
+        self.__deep_list.append(cv2.imdecode(np.frombuffer(msg.data, np.uint8),0))
+        cv2.imwrite(OUTPUT_DIR+'depth_front_'+str(int(1000*datetime.now().time()))+'.jpg', 
+                    self.__deep_list[-1])
+        
+    def get_deep(self, x,y,w,h) -> int:
+        sum = 0
+        number +=1
+        resized_image = cv2.resize(self.__deep_list[-1], (w, h))
+        if(x<AREA_OF_SEE):
+            xStart = 0
+        else:
+            xStart = x- AREA_OF_SEE
+        if(y<AREA_OF_SEE):
+            yStart = 0
+        else:
+            yStart = y- AREA_OF_SEE
+        for i in range(AREA_OF_SEE*2+1) and x+i< w:
+            for j in range(AREA_OF_SEE*2+1) and y+j<h:
+                sum += resized_image[yStart+j][xStart+i]
+                number+=1
+        return sum/number

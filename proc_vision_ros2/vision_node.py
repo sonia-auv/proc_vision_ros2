f8b9273825d16ff20e075db2f69f52e10342a2f5
Erwanmanach <Manchoteu@aol.com>
2025-08-14 14:33:54 -0400

    protect cluster

--- proc_vision_ros2/vision_node.py
+++ proc_vision_ros2/vision_node.py
@@ -208,7 +208,7 @@ class VisionNode(Node):
             elif value > valueMax2:
                 max2 = keys
                 valueMax2 = value
-        if valueMax2 > (x2-x1)*(y2-y1)*0.1 and max1==255:
+        if valueMax2 > (x2-x1)*(y2-y1)*0.05 and max1>=250:
             return (max2/255)*15
         else:
             return (max1/255)*15

d35da3614146bf58aeddfd3e9dd49f007e06bbcb
sonia-goldfish <log.club.sonia@etsmtl.net>
2025-08-13 22:59:11 -0400

    fix and debug setup (might not work on the sub)

--- proc_vision_ros2/vision_node.py
+++ proc_vision_ros2/vision_node.py
@@ -113,9 +113,25 @@ class VisionNode(Node):
     def __img_detection(self, msg: CompressedImage, model: YOLOv8) -> DetectionArray:
         try:
             image = cv2.imdecode(np.frombuffer(msg.data, np.uint8), cv2.IMREAD_COLOR)
-            return model.detect(image, msg.header.frame_id)
-        except:
+            detections = model.detect(image, msg.header.frame_id)
+            # self.print_results(image, detections)
+            return detections
+        except Exception as e:
+            self.get_logger().info(f"Vision node failure {e}")
             detections = DetectionArray()
             detections.detected_object = []
             return detections
 
+
+    def print_results(self, img, results: DetectionArray):
+        res:Detection
+        for res in results.detected_object:
+            img_res = cv2.rectangle(img, 
+                          (int(res.top_left_x), int(res.top_left_y)), 
+                          (int(res.bottom_right_x), int(res.bottom_right_y)),
+                          (0, 255, 0),
+                          1)
+            img_res = cv2.putText(img_res, f"{res.class_name} {res.confidence:.2f}", 
+                                  (int(res.top_left_x), int(res.top_left_y-10)), 1, 1, (0, 255, 0), 1)
+        if len(results.detected_object) > 0:
+            cv2.imwrite('./image_window.jpg', img_res)

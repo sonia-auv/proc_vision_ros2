73ba543bc5b30282153a440a4adbdd48f8c9e37d
RaphLrg <raphael.largeau@gmail.com>
2025-08-16 11:42:51 -0700

    bottom cam from raw to compressed

--- proc_vision_ros2/vision_node.py
+++ proc_vision_ros2/vision_node.py
@@ -39,7 +39,7 @@ class VisionNode(Node):
         self.__front_cam_sim = self.create_subscription(CompressedImage, "proc_simulation/front/compressed", self.__img_front_callback, 10)
         # self.__front_cam_depth = self.create_subscription(Image, "zed/zed_node/depth/depth_registered", self.__depth_front_callback, 10)
         
-        self.__bottom_cam_sub = self.create_subscription(Image, "camera_array/bottom/image_raw", self.__img_bottom_callback, 10)
+        self.__bottom_cam_sub = self.create_subscription(CompressedImage, "camera_array/bottom/image_raw/compressed", self.__img_bottom_callback, 10)
         self.__bottom_cam_sim = self.create_subscription(CompressedImage, "proc_simulation/bottom/compressed", self.__img_bottom_callback, 10)
 
         model_front_name = self.get_parameter("models").get_parameter_value().string_array_value[0]

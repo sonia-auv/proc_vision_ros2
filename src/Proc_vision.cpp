a1e490b7ae1e3d0f1d24379f590e228d21275a5f
Clement <Bytecrackerr@github.com>
2025-11-21 10:48:06 -0500

    LOG-162: testing

--- src/Proc_vision.cpp
+++ src/Proc_vision.cpp
@@ -105,7 +105,7 @@ namespace proc_vision_ros2
             }
 
             response->model_name = model_name;
-            RCLCPP_INFO(this->get_logger(),  "Model " << model_name.c_str());
+            RCLCPP_INFO(this->get_logger(),  "Model %s", model_name.c_str());
         }
         catch(const std::exception& e)
         {
